fa00201bddc966e2d950d9fe31958db13a913629
Maarten v.d. Donk <mvddonk@gmail.com>
2022-01-26 06:40:23 +0100

    Coordinator

--- custom_components/brewfather/__init__.py
+++ custom_components/brewfather/__init__.py
@@ -1,64 +1,114 @@
-"""Simulates a battery to evaluate how much energy it could save."""
+"""The Candy integration."""
+from __future__ import annotations
+
+# https://github.com/robinostlund/homeassistant-volkswagencarnet/blob/master/custom_components/volkswagencarnet/__init__.py
 import logging
+from datetime import timedelta
+from typing import Union
 
-import voluptuous as vol
+# ontwikkelogmvign: https://developers.home-assistant.io/docs/development_environment/
 
-from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
-from homeassistant.const import CONF_NAME
-from homeassistant.helpers import discovery
-import homeassistant.helpers.config_validation as cv
+# voorbeeld https://github.com/black-roland/homeassistant-microsoft-todo/tree/master/custom_components/microsoft_todo
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_IP_ADDRESS, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.exceptions import ConfigEntryNotReady
 
-from .const import (
-    CONF_BATTERY,
-    CONF_BATTERY_EFFICIENCY,
-    CONF_BATTERY_MAX_DISCHARGE_RATE,
-    CONF_BATTERY_MAX_CHARGE_RATE,
-    CONF_BATTERY_SIZE,
-    CONF_ENERGY_TARIFF,
-    CONF_IMPORT_SENSOR,
-    CONF_EXPORT_SENSOR,
-    DATA_UTILITY,
-    DOMAIN,
-)
+# from .client import CandyClient, WashingMachineStatus
+
+from .const import *
 
 _LOGGER = logging.getLogger(__name__)
+PLATFORMS = ["sensor"]
 
-BATTERY_CONFIG_SCHEMA = vol.Schema(
-    vol.All(
-        {
-            vol.Required(CONF_IMPORT_SENSOR): cv.entity_id,
-            vol.Required(CONF_EXPORT_SENSOR): cv.entity_id,
-            vol.Optional(CONF_ENERGY_TARIFF): cv.entity_id,
-            vol.Optional(CONF_NAME): cv.string,
-            vol.Required(CONF_BATTERY_SIZE): vol.All(float),
-            vol.Required(CONF_BATTERY_MAX_DISCHARGE_RATE): vol.All(float),
-            vol.Optional(CONF_BATTERY_MAX_CHARGE_RATE, default=1.0): vol.All(float),
-            vol.Optional(CONF_BATTERY_EFFICIENCY, default=1.0): vol.All(float),
-        },
-    )
-)
 
-CONFIG_SCHEMA = vol.Schema(
-    {DOMAIN: vol.Schema({cv.slug: BATTERY_CONFIG_SCHEMA})}, extra=vol.ALLOW_EXTRA
-)
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Setup our skeleton component."""
+    # States are in the format DOMAIN.OBJECT_ID.
+    _LOGGER.debug("username %s", config_entry.data[CONF_USERNAME])
+    _LOGGER.debug("password %s", config_entry.data[CONF_PASSWORD])
+    _LOGGER.debug("config_entry.entry_id %s", config_entry.entry_id)
+    hass.states.async_set("brewfather.Hello_World", "Works!2")
 
-async def async_setup(hass, config):
-    """Set up a battery"""
-    hass.data[DATA_UTILITY] = {}
+    update_interval = timedelta(seconds=5)
+    coordinator = BrewfatherCoordinator(hass, config_entry, update_interval)
 
-    for battery, conf in config.get(DOMAIN).items():
-        _LOGGER.debug("Setup %s.%s", DOMAIN, battery)
+    await coordinator.async_refresh()
 
-        hass.data[DATA_UTILITY][battery] = conf
+    if not coordinator.last_update_success:
+        raise ConfigEntryNotReady
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][config_entry.entry_id] = {COORDINATOR: coordinator}
+    _LOGGER.debug("Done %s", hass.data[DOMAIN])
 
+    for component in PLATFORMS:
+        _LOGGER.info("Setting up platform: %s", component)
         hass.async_create_task(
-            discovery.async_load_platform(
-                hass,
-                SENSOR_DOMAIN,
-                DOMAIN,
-                [{CONF_BATTERY: battery, CONF_NAME: conf.get(CONF_NAME, battery)}],
-                config,
-            )
+            hass.config_entries.async_forward_entry_setup(config_entry, component)
         )
+
+    # Return boolean to indicate that initialization was successfully.
     return True
 
+
+def update_callback(hass, coordinator):
+    _LOGGER.debug("CALLBACK!")
+    hass.async_create_task(coordinator.async_request_refresh())
+
+
+class BrewfatherCoordinator(DataUpdateCoordinator):
+    """Class to manage fetching data from the API."""
+
+    def __init__(self, hass: HomeAssistant, entry, update_interval: timedelta):
+        self.entry = entry
+        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=update_interval)
+
+    async def _async_update_data(self):
+        """Update data via library."""
+        _LOGGER.debug("BrewfatherCoordinator._async_update_data!")
+        # https://github.com/djtimca/HASpaceX/blob/master/custom_components/spacex/__init__.py
+        return {"status": "ok"}
+        # vehicle = await self.update()
+
+    # if not vehicle:
+    #     raise UpdateFailed(
+    #         "Failed to update WeConnect. Need to accept EULA? Try logging in to the portal: https://www.portal.volkswagen-we.com/"
+    #     )
+
+    # if self.entry.options.get(CONF_REPORT_REQUEST, False):
+    #     await self.report_request(vehicle)
+
+    # # Backward compatibility
+    # default_convert_conf = get_convert_conf(self.entry)
+
+    # convert_conf = self.entry.options.get(
+    #     CONF_CONVERT, self.entry.data.get(CONF_CONVERT, default_convert_conf)
+    # )
+
+    # dashboard = vehicle.dashboard(
+    #     mutable=self.entry.data.get(CONF_MUTABLE),
+    #     spin=self.entry.data.get(CONF_SPIN),
+    #     miles=convert_conf == CONF_IMPERIAL_UNITS,
+    #     scandinavian_miles=convert_conf == CONF_SCANDINAVIAN_MILES,
+    # )
+
+    # return dashboard.instruments
+
+    async def update(self) -> bool:  # Union[bool, Vehicle]:
+        """Update status from Volkswagen WeConnect"""
+        _LOGGER.debug("BrewfatherCoordinator.update!")
+
+        return True
+        # # update vehicles
+        # if not await self.connection.update():
+        #     _LOGGER.warning("Could not query update from volkswagen WeConnect")
+        #     return False
+
+        # _LOGGER.debug("Updating data from volkswagen WeConnect")
+        # for vehicle in self.connection.vehicles:
+        #     if vehicle.vin.upper() == self.vin:
+        #         return vehicle
+
+        # return False
--- custom_components/brewfather/sensor.py
+++ custom_components/brewfather/sensor.py
@@ -1,6 +1,14 @@
 """Platform for sensor integration."""
 from __future__ import annotations
 import logging
+import sys
+import aiohttp
+import async_timeout
+from datetime import timedelta
+from config.custom_components.brewfather import BrewfatherCoordinator
+
+from homeassistant.config_entries import ConfigEntry
+from .const import *
 
 from homeassistant.components.sensor import (
     SensorDeviceClass,
@@ -11,31 +19,266 @@ from homeassistant.const import TEMP_CELSIUS
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
 
 _LOGGER = logging.getLogger(__name__)
+REQUEST_TIMEOUT = 10
+BKK_URI = "https://api.brewfather.app/v1/batches/MdygaYwzcjEGmDTwQXJ4Wfhjbm0O8s/"
 
 
-def setup_platform(
-    hass: HomeAssistant,
-    config: ConfigType,
-    add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None,
-) -> None:
-    """Set up the sensor platform."""
-    add_entities([BrewfatherSensor()])
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+):
+    """Set up the sensor platforms."""
+    _LOGGER.debug("async_setup_entry")
+    coordinator = hass.data[DOMAIN][entry.entry_id][COORDINATOR]
+    sensors = []
+
+    sensors.append(
+        SpaceXSensor(
+            coordinator,
+            "Starman Speed",
+            "spacex_starman_speed",
+            "mdi:account-star",
+            "spacexstarman",
+        )
+    )
+    async_add_entities(sensors)
 
 
-class BrewfatherSensor(SensorEntity):
-    """Representation of a Sensor."""
+class SpaceXSensor(CoordinatorEntity):
+    """Defines a SpaceX Binary sensor."""
 
-    _attr_name = "BrewfatherSensor Temperature"
-    _attr_native_unit_of_measurement = TEMP_CELSIUS
-    _attr_device_class = SensorDeviceClass.TEMPERATURE
-    _attr_state_class = SensorStateClass.MEASUREMENT
+    def __init__(
+        self,
+        coordinator: BrewfatherCoordinator,
+        name: str,
+        entity_id: str,
+        icon: str,
+        device_identifier: str,
+    ):
+        """Initialize Entities."""
 
-    def update(self) -> None:
-        """Fetch new state data for the sensor.
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        _LOGGER.info("Migrating maarten test brewfatherssss .")
-        self._attr_native_value = 2321
+        super().__init__(coordinator=coordinator)
+
+        self._name = name
+        self._unique_id = f"spacex_{entity_id}"
+        self._state = None
+        self._icon = icon
+        self._kind = entity_id
+        self._device_identifier = device_identifier
+        self._unit_of_measure = None
+        self.attrs = {}
+
+        # if self._kind == "spacex_starman_speed":
+        #     self._unit_of_measure = SPEED_KILOMETERS_PER_HOUR
+
+        # elif self._kind == "spacex_starman_distance":
+        #     self._unit_of_measure = LENGTH_KILOMETERS
+
+    @property
+    def unique_id(self):
+        """Return the unique Home Assistant friendly identifier for this entity."""
+        return self._unique_id
+
+    @property
+    def name(self):
+        """Return the friendly name of this entity."""
+        return self._name
+
+    @property
+    def icon(self):
+        """Return the icon for this entity."""
+        return self._icon
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement for this entity."""
+        return self._unit_of_measure
+
+    # @property
+    # def device_info(self):
+    #     """Define the device based on device_identifier."""
+
+    #     device_name = "SpaceX Launches"
+    #     device_model = "Launch"
+
+    #     if self._device_identifier != "spacexlaunch":
+    #         device_name = "SpaceX Starman"
+    #         device_model = "Starman"
+
+    #     return {
+    #         ATTR_IDENTIFIERS: {(DOMAIN, self._device_identifier)},
+    #         ATTR_NAME: device_name,
+    #         ATTR_MANUFACTURER: "SpaceX",
+    #         ATTR_MODEL: device_model,
+    #     }
+
+    @property
+    def state(self):
+        """Return the state."""
+        coordinator_data = self.coordinator.data
+        # starman_data = coordinator_data["starman"]
+        # launch_data = coordinator_data["next_launch"]
+        # latest_launch_data = coordinator_data["latest_launch"]
+
+        self._state = "donky"
+
+        return self._state
+
+    async def async_update(self):
+        """Update SpaceX Binary Sensor Entity."""
+        await self.coordinator.async_request_refresh()
+        _LOGGER.debug("Updating state of the sensors.")
+
+    async def async_added_to_hass(self):
+        """Subscribe to updates."""
+        self.async_on_remove(
+            self.coordinator.async_add_listener(self.async_write_ha_state)
+        )
+
+
+# # https://developers.home-assistant.io/docs/integration_fetching_data/#separate-polling-for-each-individual-entity
+# async def async_setup_platform(
+#     hass: HomeAssistant,
+#     config: ConfigType,
+#     async_add_entities: AddEntitiesCallback,
+#     discovery_info: DiscoveryInfoType | None = None,
+# ) -> None:
+#     """Set up the sensor platform."""
+
+#     # host = config[CONF_HOST]
+
+#     async def async_update_data():
+#         _LOGGER.info("update data")
+#         user = "v50ynXGwmlYgoysyyGqpRoKFJbS2"
+#         password = "SAc0UHYsAn1BTIsDUyzMXH54UsjTEi8UMqwvlUEOCCP1HQMP7x6Ve0WIGEHliNFr"
+
+#         async with aiohttp.ClientSession() as session:
+#             async with session.get(
+#                 BKK_URI, auth=aiohttp.BasicAuth(user, password)
+#             ) as response:
+
+#                 if response.status == 200:
+#                     return await response.json()
+#                 else:
+#                     raise UpdateFailed(
+#                         f"Error communicating with API: {response.status}"
+#                     )
+
+#         # websession = async_get_clientsession(self.hass)
+
+#         # with async_timeout.timeout(REQUEST_TIMEOUT):
+#         #     response = await websession.get(
+#         #         BKK_URI, auth=aiohttp.BasicAuth(user, password)
+#         #     )
+#         # if response.status == 200:
+#         #     return await response.json()
+#         # else:
+#         #     raise UpdateFailed(f"Error communicating with API: {response.status}")
+
+#         # json_response = await resp.json()
+
+#         # async with aiohttp.ClientSession() as session:
+#         #     async with session.get(BKK_URI) as response:
+
+#         #         if response.status == 200:
+#         #             return await response.json()
+#         #         else:
+#         #             raise UpdateFailed(
+#         #                 f"Error communicating with API: {response.status}"
+#         #             )
+
+#     # async def async_update_data():
+#     #     """Fetch data from API endpoint.
+
+#     #     This is the place to pre-process the data to lookup tables
+#     #     so entities can quickly look up their data.
+#     #     """
+#     #     _LOGGER.info("brewie async_update_data .")
+
+#     #     # try:
+#     #     #     # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+#     #     #     # handled by the data update coordinator.
+#     #     #     async with async_timeout.timeout(10):
+#     #     #         return await api.fetch_data()
+#     #     # except ApiAuthError as err:
+#     #     #     # Raising ConfigEntryAuthFailed will cancel future updates
+#     #     #     # and start a config flow with SOURCE_REAUTH (async_step_reauth)
+#     #     #     raise ConfigEntryAuthFailed from err
+#     #     # except ApiError as err:
+#     #     #     raise UpdateFailed(f"Error communicating with API: {err}")
+
+#     # https://github.com/robinostlund/homeassistant-volkswagencarnet/blob/master/custom_components/volkswagencarnet/__init__.py
+#     coordinator = DataUpdateCoordinator(
+#         hass,
+#         _LOGGER,
+#         # Name of the data. For logging purposes.
+#         name="sensor",
+#         update_method=async_update_data,
+#         # Polling interval. Will only be polled if there are subscribers.
+#         update_interval=timedelta(seconds=10),
+#     )
+
+#     #  await coordinator.async_config_entry_first_refresh()
+#     await coordinator.async_refresh()
+
+#     async_add_entities(
+#         [
+#             BrewfatherSensor(coordinator)
+#         ]  # , idx) for idx, ent in enumerate(coordinator.data)
+#     )
+#     # add_entities([BrewfatherSensor(coordinator)])
+
+
+# # run: hass -c ./config
+# class BrewfatherSensor(CoordinatorEntity, SensorEntity):
+#     """Representation of a Sensor."""
+
+#     def __init__(self, coordinator):  # , idx):
+#         """Pass coordinator to CoordinatorEntity."""
+#         super().__init__(coordinator)
+#         # self.idx = idx
+
+#     _attr_name = "BrewfatherSensor Temperature"
+#     _attr_native_unit_of_measurement = TEMP_CELSIUS
+#     _attr_device_class = SensorDeviceClass.TEMPERATURE
+#     _attr_state_class = SensorStateClass.MEASUREMENT
+
+#     async def async_added_to_hass(self):
+#         _LOGGER.info("Migrating dsadsa2 test brewfather .")
+
+#     async def async_update(self) -> None:
+#         """Fetch new state data for the sensor.
+#         This is the only method that should fetch new data for Home Assistant.
+#         """
+#         _LOGGER.info("Migrating maarten test brewfather .")
+#         # self._attr_native_value = 23
+#         # try:
+#         #     websession = async_get_clientsession(self.hass)
+#         #     user = "v50ynXGwmlYgoysyyGqpRoKFJbS2"
+#         #     password = (
+#         #         "SAc0UHYsAn1BTIsDUyzMXH54UsjTEi8UMqwvlUEOCCP1HQMP7x6Ve0WIGEHliNFr"
+#         #     )
+#         #     with async_timeout.timeout(REQUEST_TIMEOUT):
+#         #         resp = await websession.get(
+#         #             BKK_URI, auth=aiohttp.BasicAuth(user, password)
+#         #         )
+#         #     if resp.status != 200:
+#         #         _LOGGER.error(f"{resp.url} returned {resp.status}")
+#         #         return
+
+#         #     json_response = await resp.json()
+#         #     _LOGGER.debug("async_update: %s", json_response)
+#         #     # _LOGGER.debug("async_update: %s", resp.text().encode("utf-8"))
+#         #     return json_response
+
+#         # except Exception as e:
+#         #     _LOGGER.error(
+#         #         "[" + sys._getframe().f_code.co_name + "] Exception: " + str(e)
+#         #     )
